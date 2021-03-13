dccaa21961578f6b620ca72a2bdcaec9c6b6e397
lizhenjiang01 <lizhenjiang01@58.com>
2021-03-14 00:16:44 +0800

    add b combine

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ class A {
         this.setState({
             a: function () {
                 console.log('22222');
+                console.log('1111');
+                console.log('33');
             }
         })
     }
